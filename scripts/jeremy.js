fc1ff2e4988bc25fae8cabcfcfa33c04f0e0a3fc
Jeremy White <jcwhite414@gmail.com>
2022-08-06 13:00:33 -0500

    added initial project files

---
+++ scripts/jeremy.js
@@ -0,0 +1,17 @@
+const jeremy = [
+    {
+        orderNumber: 101,
+        tableID: 1,
+        tableOrder: [
+            {
+                guestSeat: 1,
+                drink: "water",
+                appetizer: "Curly Fries",
+                mainCourse: "Chicken Wings",
+                rewardsCustomer: true
+            }
+        ]
+    }
+]
+
+// console.log("hello World")
