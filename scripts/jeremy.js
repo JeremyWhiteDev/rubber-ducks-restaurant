cb41ac13faf1a731bafe63f2ef39d326984e2f4d
Jeremy White <jcwhite414@gmail.com>
2022-08-08 21:01:34 -0500

    added functioning loops to jeremy.js, and styling to main.css. Not ready to merge

--- scripts/jeremy.js
+++ scripts/jeremy.js
@@ -1,6 +1,42 @@
 const jeremy = [
     {
         orderNumber: 101,
+        orderStatus: "Complete",
+        tableID: 1,
+        tableOrder: [
+            {
+                guestSeat: 1,
+                drink: "Water",
+                appetizer: "Curly Fries",
+                mainCourse: "Chicken Wings",
+                rewardsCustomer: true
+            },
+            {
+                guestSeat: 2,
+                drink: "Coke",
+                appetizer: "Curly Fries",
+                mainCourse: "Chicken Wings",
+                rewardsCustomer: true
+            },
+            {
+                guestSeat: 3,
+                drink: "Water No Lemon",
+                appetizer: "Curly Fries",
+                mainCourse: "Chicken Wings",
+                rewardsCustomer: false
+            },
+            {
+                guestSeat: 4,
+                drink: "Water No Lemon",
+                appetizer: "Curly Fries",
+                mainCourse: "Chicken Wings",
+                rewardsCustomer: false
+            }
+        ]
+    },
+    {
+        orderNumber: 102,
+        orderStatus: "Active",
         tableID: 1,
         tableOrder: [
             {
@@ -29,6 +65,7 @@ const jeremy = [
     {
         orderNumber: 201,
         tableID: 2,
+        orderStatus: "Active",
         tableOrder: [
             {
                 guestSeat: 1,
@@ -56,6 +93,7 @@ const jeremy = [
     {
         orderNumber: 301,
         tableID: 3,
+        orderStatus: "Active",
         tableOrder: [
             {
                 guestSeat: 1,
@@ -97,11 +135,28 @@ let waiterSection = "";
 // main courses: meal(seat Number)
 
 
+for (const bananas of jeremy) {
+    console.log(bananas.orderNumber)
+    waiter = waiter + bananas.orderNumber;
+}
+
+
 for (const table of jeremy) {
-     waiterSection += `
-     <section>
-     <div><h1>Order #:${table.orderNumber}</h1>
-     <h3>Table#:${table.tableID}</h3></div>
+    //if order is complete, then add section with additional "complete" class with different styling"
+     if (table.orderStatus === "Complete") {
+        waiterSection += `
+     <section class ="jeremy-order complete">`
+
+    //else, just add section with "jeremy-order"
+    } else {
+        waiterSection += `
+     <section class ="jeremy-order">`}
+
+     // add div for header of card and start ul for drinks
+    waiterSection +=`
+     <div class="jeremy-header"><h1>Order #${table.orderNumber}</h1>
+     <h3>Table #${table.tableID}</h3><h3>Status: ${table.orderStatus}</div>
+     
      <ul><h3>Drinks:</h3>`;
    
      for (let i = 0; i < table.tableOrder.length; i++) {
@@ -133,4 +188,5 @@ for (const table of jeremy) {
 }
 console.log(waiterSection);
 
-document.getElementById("jeremy").innerHTML = waiterSection;
+// document.getElementById("jeremy").innerHTML = waiterSection;
+
