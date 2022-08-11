0152c4afadf314faab853a79ea819314cc350da2
JeremyK1830 <108768065+JeremyK1830@users.noreply.github.com>
2022-08-10 21:59:04 -0500

    jt js file worked along with html and css

---
+++ scripts/jt.js
@@ -0,0 +1,161 @@
+const jt = [
+    {
+            
+        orderNumber: 701,
+            orderStatus: "Complete",
+            tableID: 7,
+            tableOrder: [
+            {
+                
+                guestSeat: 1,
+                drink: "Sweet Tea",
+                side: "French Fries",
+                mainCourse: "Hot Chicken Wings",
+                rewardsCustomer: true
+            },
+            {
+                
+                guestSeat: 1,
+                drink: "Sweet Tea",
+                side: "French Fries",
+                mainCourse: "Mild Wings",
+                rewardsCustomer: true 
+            }
+        ]
+            },
+            {
+            
+            orderNumber: 801,
+            orderStatus: "Complete",
+            tableID: 8,
+            tableOrder: [
+            {
+                
+                guestSeat: 1,
+                drink: "Water",
+                side: "Mac and Cheese bites",
+                mainCourse:"Medium Hot Wings",
+                rewardsCustomer:false
+            },
+            {  
+                 
+                guestSeat: 1,
+                drink: "Sweet Tea",
+                side: "French Fries",
+                mainCourse: "Mild Wings",
+                rewardsCustomer: true
+            },   
+            {  
+                 
+                guestSeat: 1,
+                drink: "Sweet Tea",
+                side: "French Fries",
+                mainCourse: "Mild Wings",
+                rewardsCustomer: true 
+            }
+        ]
+            },
+            {
+
+            orderNumber: 901,
+            orderStatus: "Not Complete",
+            tableID: 9,
+            tableOrder: [
+            {
+                     
+                    guestSeat:1,
+                    drink: "Coke a cola",
+                    side: "Cheese Fries",
+                    mainCourse: "Extremely Hot Wings",
+                    rewardsCustomer: true
+            },
+            {
+                    
+                    guestSeat: 1,
+                    drink: "Water",
+                    side: "Mac and Cheese bites",
+                    mainCourse:"Medium Hot Wings",
+                    rewardsCustomer:false
+            },
+            {
+                    
+                    guestSeat: 1,
+                    drink: "Sweet Tea",
+                    side: "French Fries",
+                    mainCourse: "Mild Wings",
+                    rewardsCustomer: true
+            },
+            {   
+                    
+                    guestSeat: 1,
+                    drink: "Sweet Tea",
+                    side: "French Fries",
+                    mainCourse: "Mild Wings",
+                    rewardsCustomer: true
+            }
+        ] 
+    }
+]          
+    
+
+let htmlSection = "";
+
+
+
+
+
+for (const table of jt) {
+ 
+     if (table.orderStatus === "Complete") {
+        htmlSection += `
+     <section class ="jt-order complete">`
+
+    
+    } else {
+        htmlSection += `
+     <section class ="jt-order">`}
+
+     
+    htmlSection +=`
+     <div class="jt-header"><h1>Order #${table.orderNumber}</h1>
+     <h3>Table #${table.tableID}</h3><h3>Status: ${table.orderStatus}</div>
+     
+     <ul><h3>Drinks:</h3>`;
+    
+    
+     for (let i = 0; i < table.tableOrder.length; i++) {
+        htmlSection += `
+        <li>${table.tableOrder[i]["drink"]}(#${table.tableOrder[i]["guestSeat"]})</li>`;
+    }
+   
+    htmlSection += `
+    </ul>`
+
+    
+     htmlSection += `
+     <ul><h3>Sides:</h3>`;
+     for (let i = 0; i < table.tableOrder.length; i++) {
+        htmlSection += `
+        <li>${table.tableOrder[i]["side"]}(#${table.tableOrder[i]["guestSeat"]})</li>`;
+    }
+
+    htmlSection += `
+    </ul>`
+
+     htmlSection += `
+    <ul><h3>Main Course:</h3>`;
+     for (let i = 0; i < table.tableOrder.length; i++) {
+        htmlSection += `
+        <li>${table.tableOrder[i]["mainCourse"]}(#${table.tableOrder[i]["guestSeat"]})</li>`;
+    }
+
+    
+    htmlSection += `
+    </ul></section>`
+     
+   
+
+}
+console.log(htmlSection);
+
+document.getElementById("jt").innerHTML = htmlSection;
