3ed0157ed38876ee60b17f8fc2d87432ce9e7484
Jeremy White <jcwhite414@gmail.com>
2022-08-12 20:03:24 -0500

    added Section Header

--- scripts/kyle.js
+++ scripts/kyle.js
@@ -46,7 +46,7 @@ const kyle = [
 ]
          
 
-let htmlSection ="" 
+let htmlSection =`<h3 class="section-header">Kyles's Dashboard</h3>`; 
 for (const order of kyle) {
 htmlSection += ` <h1>Table ${order.tableID}</h1>
 <ul>
