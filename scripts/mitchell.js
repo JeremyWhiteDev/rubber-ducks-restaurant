6b33671c5ef4ef0f84f3ce86d39cc11c52798231
Mcshelton96 <mcshelton96@gmail.com>
2022-08-22 20:18:57 -0500

    Mitchell Refactor Complete

--- scripts/mitchell.js
+++ scripts/mitchell.js
@@ -2,32 +2,32 @@ import { getWaiters } from "./data.js";
 import { getOrders } from "./data.js";
 import { getGuestOrders } from "./data.js";
 
-const mitchell = [
-     {
-        id:1,
-        orderNumber: 401,
-        tableID: 4,
-        drinks: 'the mallard, coke',
-        appetizer: 'Krinkle Cut Fries',
-        mainCourse: 'Rubber Duck Special',
-        rewardsCustomer: false,
-        tableOrderOne: 
-              {
-                guestSeat: 1,
-                drink: "The Mallard",
-                appetizer: "Krinkle Cut Fries",
-                mainCourse: "Rubber Duck Special",
-                rewardsCustomer: true  
-            },
-        tableOrderTwo:
-            {
-                guestSeat: 2,
-                drink: "Domestic Duck",
-                appetizer: "Fried Pickles",
-                mainCourse: "Chicken Tenders",
-                rewardsCustomer: false  
-            }
-    },
+// const mitchell = [
+//      {
+//         id:1,
+//         orderNumber: 401,
+//         tableID: 4,
+//         drinks: 'the mallard, coke',
+//         appetizer: 'Krinkle Cut Fries',
+//         mainCourse: 'Rubber Duck Special',
+//         rewardsCustomer: false,
+//         tableOrderOne: 
+//               {
+//                 guestSeat: 1,
+//                 drink: "The Mallard",
+//                 appetizer: "Krinkle Cut Fries",
+//                 mainCourse: "Rubber Duck Special",
+//                 rewardsCustomer: true  
+//             },
+//         tableOrderTwo:
+//             {
+//                 guestSeat: 2,
+//                 drink: "Domestic Duck",
+//                 appetizer: "Fried Pickles",
+//                 mainCourse: "Chicken Tenders",
+//                 rewardsCustomer: false  
+//             }
+    // },
     // tableFive = {
     //     id:2,
     //     orderNumber: 501,
@@ -72,24 +72,80 @@ const mitchell = [
     //             rewardsCustomer: false  
     //         }
     //       }   
-        ]
+        // ]
 //JS to HTML
+
+
+const displaymitchellsOrders = () => {
+const waiterCopy = getWaiters()
+const ordersCopy = getOrders()
+const guestCopy = getGuestOrders()
+
 
 let mitchellhtmlSection = '';
 
-for (const mitchellsOrders of mitchell) {
-    mitchellhtmlSection += `<div class="card">
-    <div class="mitchellContainer">
-        <h3 id='mh3'>Table ${mitchellsOrders.tableID}</h3>
-        <h4 id='mh4'>Orders</h4>
-        <ul id="morderItems">
-            <li id='mli'>Drink: ${mitchellsOrders.drinks}</li>
-            <li id='mli'>App: ${mitchellsOrders.appetizer} </li>
-            <li id='mli'>Main Course: ${mitchellsOrders.mainCourse}</li>
-            <li id='mli'>Rewards Customer: ${mitchellsOrders.rewardsCustomer}</li>
-        </ul>
-        
-    </div>
-</div>`
+// step one: guest orders for loop
+// step two: get each order to display on html
+// step three: reference orders for Table Ids
+
+// find waiter iD =2 
+// find id that matches order Id
+// print to html 
+
+for (const order of ordersCopy) {
+    if (order.waiterID === 2) {
+        for (const guestOrder of guestCopy) {
+            if (guestOrder.orderID === order.id) {
+                mitchellhtmlSection += `<div class="card">
+                <div class="mitchellContainer">
+                    <h3 id='mh3'>Table ${order.tableID}</h3>
+                    <h4 id='mh4'>Orders</h4>
+                    <ul id="morderItems">
+                        <li id='mli'>Drink: ${guestOrder.drinks}</li>
+                        <li id='mli'>App: ${guestOrder.appetizer} </li>
+                        <li id='mli'>Main Course: ${guestOrder.mainCourse}</li>
+                    </ul>
+                </div>
+            </div>`
+            }
+        }
+    }
 }
+
+
 document.getElementById('mitchell').innerHTML = mitchellhtmlSection
+}
+displaymitchellsOrders()
+
+// for (const mitchellsOrders of guestCopy) {
+//     mitchellhtmlSection += `<div class="card">
+//     <div class="mitchellContainer">
+//         <h3 id='mh3'>Table ${mitchellsOrders}</h3>
+//         <h4 id='mh4'>Orders</h4>
+//         <ul id="morderItems">
+//             <li id='mli'>Drink: ${mitchellsOrders.drinks}</li>
+//             <li id='mli'>App: ${mitchellsOrders.appetizer} </li>
+//             <li id='mli'>Main Course: ${mitchellsOrders.mainCourse}</li>
+//             <li id='mli'>Rewards Customer: ${mitchellsOrders.rewardsCustomer}</li>
+//         </ul>
+        
+//     </div>
+// </div>`
+// }
+
+// for (const mitchellsOrders of mitchell) {
+//     mitchellhtmlSection += `<div class="card">
+//     <div class="mitchellContainer">
+//         <h3 id='mh3'>Table ${mitchellsOrders.tableID}</h3>
+//         <h4 id='mh4'>Orders</h4>
+//         <ul id="morderItems">
+//             <li id='mli'>Drink: ${mitchellsOrders.drinks}</li>
+//             <li id='mli'>App: ${mitchellsOrders.appetizer} </li>
+//             <li id='mli'>Main Course: ${mitchellsOrders.mainCourse}</li>
+//             <li id='mli'>Rewards Customer: ${mitchellsOrders.rewardsCustomer}</li>
+//         </ul>
+        
+//     </div>
+// </div>`
+// }
+// document.getElementById('mitchell').innerHTML = mitchellhtmlSection
